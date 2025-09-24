e8e5bd0e792384eec40bd5ebdd4b3fff474d0f90
sthe-rms <20211bsifsa0028@ifba.edu.br>
2025-09-23 23:13:01 -0300

    Criação dos serviços/ autenticação para login e cadastro

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -65,7 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 10),
                 MyButton(
                   onTap: () {},
-                  text: "Entrar",
+                  text: "Entrar", 
                 ),
                 const SizedBox(height: 50),
                   Row(
---
+++ lib/services/auth/login_ou_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginOuRegister extends StatefulWidget {
+  const LoginOuRegister({super.key});
+
+  @override
+  State<LoginOuRegister> createState() => _LoginOuRegisterState();
+}
+
+class _LoginOuRegisterState extends State<LoginOuRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
